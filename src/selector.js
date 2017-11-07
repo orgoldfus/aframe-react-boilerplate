469ca409812c260e05aecabd0bf507cb44e1424e
Or Goldfus <or.goldfus@klarna.com>
2017-11-07 23:18:52 +0200

    Add visual feedback for hover

--- src/selector.js
+++ src/selector.js
@@ -17,6 +17,8 @@ export default class Selector extends React.Component {
           geometry={{primitive: 'box', depth: 0.1}}
           material={{color: this.props.color, opacity: 0.6}}
           events={{click: this.props.onClick}}
+          animation__in={{property: 'scale', startEvents: 'mouseenter', to: '1.2 1.2 1.2', dur: 300}}
+          animation__out={{property: 'scale', startEvents: 'mouseleave', to: '1 1 1', dur: 300}}
           animation__rotate={this.props.animation}>
           <Entity primitive="a-text" value={this.props.text} align="center"/>
         </Entity>
